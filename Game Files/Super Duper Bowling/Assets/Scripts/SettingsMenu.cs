1dcad9a00025f3f0d8e557775fdd4fef89d2bb5f
Brotron374 <Brotrongamingyt@gmail.com>
2025-10-31 09:11:12 +1030

    Reapply "Merge branch 'main' of https://github.com/Mxxlls/Super-Duper-Bowling"
    This reverts commit d900410246b154f2f5aa5315007c3cd4332087e1.

--- Game Files/Super Duper Bowling/Assets/Scripts/SettingsMenu.cs
+++ Game Files/Super Duper Bowling/Assets/Scripts/SettingsMenu.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public TMP_Dropdown resolutionDropdown;
+    public moveAdvanced moveAdvanced; // for FOV
+    public TextMeshProUGUI fovText;
+    public Slider FOVslider;
     Resolution[] resolutions;
     void Start()
     {
@@ -41,5 +45,11 @@ public class SettingsMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetFOV()
+    {
+        moveAdvanced.FOV = FOVslider.value;
+        fovText.text = ("FOV: " + moveAdvanced.FOV);
     }
 }
